1638da721e2326f8b54c7b08f936c243407a03d0
cuong <cuonglthe173588@fpt.edu.vn>
2024-06-18 01:05:32 +0700

    first commit

---
+++ src/main/java/com/example/Controller/OssGr5Application.java
@@ -0,0 +1,13 @@
+package com.example.Controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OssGr5Application {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello world");;
+	}
+
+}
