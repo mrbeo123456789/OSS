53d7f4131289108c744ef36e84dd966e520e9d38
CanhDuc710 <canhvdhe170639@fpt.edu.vn>
2024-06-24 23:43:23 +0700

    adding regiter

--- src/main/java/com/oss/service/userservice.java
+++ src/main/java/com/oss/service/userservice.java
@@ -1,18 +1,42 @@
 package com.oss.service;
 
+import com.oss.dto.UserRegistrationDto;
 import com.oss.model.User;
 import com.oss.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public class userservice {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+
+    public User registerNewUser(UserRegistrationDto userDto) {
+        if (userRepository.findByEmail(userDto.getEmail()) != null) {
+            throw new RuntimeException("Email đã được đăng ký");
+        }
+
+        String hashedPassword = passwordEncoder.encode(userDto.getPassword());
+
+        User newUser = new User(
+                userDto.getFullName(),
+                userDto.getUsername(),
+                userDto.getEmail(),
+                userDto.getMobile(),
+                hashedPassword
+        );
+
+        return userRepository.save(newUser);
+    }
+
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
-
 }
